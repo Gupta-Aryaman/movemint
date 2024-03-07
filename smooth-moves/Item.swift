c9f8b0d3551fdaf34f821c56d9b989d6f56e6349
vxibh <vxx6hxv@gmail.com>
2024-03-07 17:42:26 +0530

    App Interface

---
+++ smooth-moves/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  smooth-moves
+//
+//  Created by Vaibhav Sharma on 27/02/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
